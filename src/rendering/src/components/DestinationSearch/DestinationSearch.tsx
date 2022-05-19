f569143202c69a5e95433d1d0ead0c0ff700419b
nick-mattocks <64479075+nick-mattocks@users.noreply.github.com>
2022-05-19 14:51:21 +0100

    Feature/create destination component (#7)
    * Created new search box component with no client side functionality yet
    
    * On-going dev on destination component
    
    * Completed initial destination search component
    
    * Disable eslint globally.  Try add some better TS in!
    
    Co-authored-by: Nicholas Mattocks <nicholas.mattocks@jet2.com>

---
+++ src/rendering/src/components/DestinationSearch/DestinationSearch.tsx
@@ -0,0 +1,19 @@
+import { Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+import AutoCompleteBox from './AutoCompleteBox';
+
+type DestinationSearchProps = ComponentProps & {
+  fields: {
+    DestinationLabel: Field<string>;
+    SearchBoxPlaceholderText: Field<string>;
+  };
+};
+
+const DestinationSearch = ({ fields }: DestinationSearchProps): JSX.Element => (
+  <div>
+    <label>{fields.DestinationLabel.value}</label>
+    <AutoCompleteBox phText={fields.SearchBoxPlaceholderText.value} />
+  </div>
+);
+
+export default withDatasourceCheck()<DestinationSearchProps>(DestinationSearch);
