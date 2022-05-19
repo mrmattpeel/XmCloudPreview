432fd7f4a6f7108a497dbc615386c3cc3d14287d
Nicholas Mattocks <nicholas.mattocks@jet2.com>
2022-05-19 16:24:44 +0100

    Sitecoreified the Hub Bar component

---
+++ src/rendering/src/components/HubBar/HubBar.tsx
@@ -0,0 +1,36 @@
+import { withDatasourceCheck, Field, Item } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+import HubBarItem from './HubBarItem';
+import Link from 'next/link';
+import React from 'react';
+
+type HubBarProps = ComponentProps & {
+  fields: {
+    Links: Link[];
+  };
+};
+
+type Link = ComponentProps & {
+  fields: {
+  Title: Field<string>;
+  Href: any;
+  }
+}
+
+const HubBar = ({ fields }: HubBarProps): JSX.Element => (
+  <div className="hub">
+    <div className="close-menu-overlay"></div>
+    <nav className="hub-container container">
+      <ul className="hub-nav-menu hub-wrapper">
+        {
+          React.Children.toArray(
+          fields.Links.map((link: Link) => {
+            return(<HubBarItem itemName={link.fields.Title.value} itemUrl={link.fields.Href.value.href} />)
+          }))
+        }
+      </ul>
+    </nav>
+  </div>
+);
+
+export default withDatasourceCheck()<HubBarProps>(HubBar);
