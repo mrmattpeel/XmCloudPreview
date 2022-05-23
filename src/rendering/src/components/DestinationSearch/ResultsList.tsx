2b5f438727fe1d0dda6c634aa4d3a40ed4215ec3
Nicholas Mattocks <nicholas.mattocks@jet2.com>
2022-05-23 12:38:37 +0100

    Updated CSS for destination list

--- src/rendering/src/components/DestinationSearch/ResultsList.tsx
+++ src/rendering/src/components/DestinationSearch/ResultsList.tsx
@@ -6,13 +6,21 @@ const ResultsList = (props: any) => {
   };
 
   return hasData(props.searchResults) ? (
-    <ul>
+    <div className='search-nav-input__autocomplete-wrapper'>
+    <ul className="ui-menu ui-widget ui-widget-content ui-autocomplete ui-front">
       {React.Children.toArray(
       props.searchResults.map((destination: string) => {
-        return <li>{destination}</li>;
+        return <li className='ui-menu-item'>
+          <div className='ui-menu-item-wrapper ui-menu-item-wrapper--destination'>
+          <p className='ui-menu-item-title'>
+          {destination}
+          </p>
+          </div>
+          </li>;
       })
       )}
     </ul>
+    </div>
   ) : (
     <div>
       <em>No destinations found!</em>
